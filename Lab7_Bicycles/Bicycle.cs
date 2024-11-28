46c0e5f7563f0237a9bcfdeb4e88276e751dbf2a
Maks Kond <lyubar06m@gmail.com>
2024-11-28 17:12:28 +0200

    Add project files.

---
+++ Lab7_Bicycles/Bicycle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab7_Bicycles;
+
+
+namespace Lab7_Bicycles
+{
+    public class Bicycle : IComparable<Bicycle>
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public double Weight { get; set; }
+        public int Gears { get; set; }
+        public double Mileage { get; set; }
+        public double Distance { get; set; }
+        public double Time { get; set; }
+
+        // Конструктор
+        public Bicycle(string brand, string model, string type, double weight, int gears, double mileage, double distance, double time)
+        {
+            Brand = brand;
+            Model = model;
+            Type = type;
+            Weight = weight;
+            Gears = gears;
+            Mileage = mileage;
+            Distance = distance;
+            Time = time;
+        }
+
+        // Метод для обчислення швидкості
+        public double Speed => Distance / Time;
+
+        // Реалізація методу ToString для відображення інформації про велосипед
+        public override string ToString()
+        {
+            return $"Brand: {Brand}, Model: {Model}, Type: {Type}, Weight: {Weight}, Gears: {Gears}, Mileage: {Mileage}, Distance: {Distance}, Time: {Time}, Speed: {Speed:F2}";
+        }
+
+        // Реалізація методу CompareTo для порівняння велосипедів (за Mileage, наприклад)
+        public int CompareTo(Bicycle other)
+        {
+            if (other == null)
+                return 1;
+
+            // Сортуємо за пробігом (Mileage)
+            return this.Mileage.CompareTo(other.Mileage);
+        }
+    }
+
+}
---
+++ Lab7_Bicycles/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_Bicycles
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bicycles.bin");
+
+            // Читання об'єктів Bicycle з бінарного файлу
+            List<Bicycle> bicycles = ReadBicyclesFromBinaryFile(filePath);
+
+            Console.WriteLine("Дані перед сортуванням:");
+            foreach (var bicycle in bicycles)
+            {
+                Console.WriteLine(bicycle);
+            }
+
+            // Сортування за пробігом (Mileage) за зростанням
+            bicycles.Sort();
+
+            Console.WriteLine("\nДані після сортування:");
+            foreach (var bicycle in bicycles)
+            {
+                Console.WriteLine(bicycle);
+            }
+        }
+
+        static List<Bicycle> ReadBicyclesFromBinaryFile(string filePath)
+        {
+            var bicycles = new List<Bicycle>();
+
+            if (File.Exists(filePath))
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    while (fs.Position < fs.Length)
+                    {
+                        try
+                        {
+                            // Читання даних з файлу в тому ж порядку, як вони були записані
+                            string brand = reader.ReadString();
+                            string model = reader.ReadString();
+                            string type = reader.ReadString();
+                            double weight = reader.ReadDouble();
+                            int gears = reader.ReadInt32();
+                            double mileage = reader.ReadDouble();
+                            double distance = reader.ReadDouble();
+                            double time = reader.ReadDouble();
+
+                            // Додавання об'єкта Bicycle у список
+                            bicycles.Add(new Bicycle(brand, model, type, weight, gears, mileage, distance, time));
+                        }
+                        catch (EndOfStreamException)
+                        {
+                            // Якщо кінець потоку, то завершуємо читання
+                            Console.WriteLine("Досягнуто кінець файлу.");
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            // Логування помилок
+                            Console.WriteLine($"Помилка при читанні даних: {ex.Message}");
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не знайдено за шляхом: " + filePath);
+            }
+
+            return bicycles;
+        }
+
+
+    }
+}
+
